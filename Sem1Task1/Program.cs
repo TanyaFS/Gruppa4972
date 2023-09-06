891b224329260de0bc31ab069762c8fb2dc48620
Tanya Fech <t_fech@mail.ru>
2023-09-06 18:04:42 +0300

    Added solutions for Task1,3,5

---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача №1
+// Напишите программу, которая на вход принимает
+// два числа и проверяет, является ли первое число квадратом второго.
+
+Console.WriteLine("Введите число 1: ");
+int num1 = int.Parse(Console.ReadLine()??"0");
+
+Console.WriteLine("Введите число 2: ");
+int num2 = int.Parse(Console.ReadLine()??"0");
+
+//Вариант 1: условие? вариант1 : вариант2
+//Console.WriteLine(num1 == Math.Pow(num2, 2) ? "Да" : "Нет");
+
+//Вариант 2:
+if (num1 == Math.Pow(num2, 2))
+{
+    Console.WriteLine("Да"); 
+}
+else
+{
+    Console.WriteLine("Нет"); 
+}
