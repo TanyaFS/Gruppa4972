88dfc899465d749d6ea1d6190ee65cce1346f875
Tanya Fech <t_fech@mail.ru>
2023-09-15 19:46:42 +0300

    Added additional solutions to tasks 19,21,23

--- Sem3Task21/Program.cs
+++ Sem3Task21/Program.cs
@@ -9,7 +9,7 @@
 }*/
 
 
-using System;
+/*using System;
 
 public class Answer
 {
@@ -47,4 +47,35 @@ public class Answer
         double result = Length(new int[]{x1, x2, x3}, new int[]{y1, y2, y3});
         Console.WriteLine($"{result:F2}");
     }
+}*/
+
+// *Осуществить возможность ввода всех точек в одну строку
+// *Вывод сообщения и вызов метода DistanceCalculation
+
+double Prompt(string message)
+{
+    Console.WriteLine(message);
+    string values = Console.ReadLine()??",";
+
+    string[] valuesArray = values.Split(",");
+    int xA = Convert.ToInt32(valuesArray[0]);
+    int yA = Convert.ToInt32(valuesArray[1]);
+    int zA = Convert.ToInt32(valuesArray[2]);
+    int xB = Convert.ToInt32(valuesArray[3]);
+    int yB = Convert.ToInt32(valuesArray[4]);
+    int zB = Convert.ToInt32(valuesArray[5]);
+
+    return DistanceCalculation(xA, yA, zA, xB, yB, zB);
 }
+
+
+//A (3,6,8); B (2,1,-7)
+// Расчёт расстояния
+double DistanceCalculation(int xA, int yA, int zA, int xB, int yB, int zB)
+{
+    return Math.Sqrt(Math.Pow(Math.Abs(xA - xB), 2)
+                     + Math.Pow(Math.Abs(yA - yB), 2)
+                     + Math.Pow(Math.Abs(zA - zB), 2));
+}
+
+Console.WriteLine(Prompt("Введите 6 чисел через запятую (xA,yA,zA,xB,yB,zB): "));
