5a20e0d1889b8581dc12cc9d8d00a102a0b3d161
Tanya Fech <t_fech@mail.ru>
2023-09-20 17:53:50 +0300

    Additional solutions to tasks 25,27,29

--- Sem4Task29/Program.cs
+++ Sem4Task29/Program.cs
@@ -2,7 +2,8 @@
 // выводит их на экран.
 
 // Вызываем метод GenArr для создания массива из 8 случайных чисел
-int[] numbers = GenArr(8);
+
+/*int[] numbers = GenArr(8);
 {
     // Формируем строку, объединяя элементы массива через запятую и заключая их в квадратные скобки
     string output = "[" + string.Join(", ", numbers) + "]";
@@ -18,12 +19,32 @@ int[] GenArr(int length)
 
     for (int i = 0; i < length; i++)
     {
-        array[i] = random.Next(0,9); // Генерируем случайное целое число
+        array[i] = random.Next(0,10); // Генерируем случайное целое число
     }
 
     return array;
+}*/
+
+// * Написать программу которая из имен через запятую выберет случайное имя и выведет в терминал
+
+// Вывод сообщения и запись введённых данных
+string[] Prompt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine()??",";
+    string[] names = value.Replace(".", ",").Replace(" ", "").Split(",");
+
+    return names;
+}
+// Выбирает случайный индекс массива
+int RandomName(int length)
+{
+    return new Random().Next(0, length);
 }
 
+string[] names = Prompt("Введите имена через запятую или точку: ");
+
+Console.WriteLine(names[RandomName(names.Length)]);
 
 
 
