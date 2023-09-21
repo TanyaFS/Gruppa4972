700a32c1924f6a7f07b7383116eb6e2a71965cf7
Tanya Fech <t_fech@mail.ru>
2023-09-21 18:41:33 +0300

    Solutions to Sem5 tasks 31,32,33,35,37

---
+++ Sem5Task33/Program.cs
@@ -0,0 +1,97 @@
+﻿// №33 Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+
+// Вариант 1
+// Вывод сообщения и запись введённых данных
+/*
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine()??",";
+    int number = Convert.ToInt32(value);
+
+    return number;
+}
+
+// Проверяет, есть ли число
+string Check(int[] array, int num)
+{
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] == num)
+            return $"Есть, индекс {i}";
+    return "Нет";
+}
+
+// Выводит элементы массива в консоль
+void OutputArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+        Console.Write(array[i] + ", ");
+    Console.WriteLine(array[array.Length - 1] + "]");
+}
+
+
+int[] array = {1, -2, 5, 6, 58, 7, -3, -42, 63, 15};
+OutputArray(array);
+
+int element = Prompt("Введите число для поиска: ");
+
+Console.WriteLine(Check(array, element));
+*/
+
+// Вариант 2
+
+int[] GenArray(int length, int lowBorder, int highBorder)
+{
+    int[] arr = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(lowBorder, highBorder + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write($"[ ");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.Write(arr[arr.Length - 1] + " ]");
+    System.Console.WriteLine();
+}
+
+int FindElm(int[] arr, int elm)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == elm)
+        {
+            return i;
+        }
+    }
+    return -1;
+}    
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int elm = int.Parse(Console.ReadLine() ?? "0");
+    return elm;
+}
+
+int[] arr = GenArray(12,-9,9);
+Console.WriteLine("Сгенерированный массив:");
+PrintArray(arr);
+int numberToFind = ReadData("Введите число для поиска: ");
+int index = FindElm(arr, numberToFind);
+if (index != -1)
+{
+    Console.WriteLine($"Число {numberToFind} присутствует в массиве на позиции {index}.");
+}
+else
+{
+    Console.WriteLine($"Число {numberToFind} отсутствует в массиве.");
+}
---
+++ Sem5Task35/Program.cs
@@ -0,0 +1,75 @@
+﻿// №35 Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива,значения которых лежат в отрезке [10,99].
+
+//Заполняем одномерный массив
+int[] GenArray(int num, int lowBorder, int highBorder)
+{
+    int[] array = new int[num];
+
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = new Random().Next(lowBorder, highBorder + 1);
+    }
+    return array;
+}
+
+// Печатаем результат
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+// Находим количество элементов в заданном диапазоне
+
+// Вариант 1
+/*int CountElm(int[] arr, int x, int y)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= x && arr[i] <= y)
+        {
+            count++;
+        }
+    }
+    return count;
+}*/
+
+// Вариант 2
+int CountElm(int[] arr, int elm)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+//Печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+// Вызываем генерацию массива, выводим изначальный массив, инвертируем массив и выводим инвертированный массив
+int[] array = GenArray(123, 0, 1000);
+PrintArray(array);
+
+// Вариант 1
+//int x = 10;
+//int y = 99;
+//int count = CountElm(array, x, y);
+
+// Вариант 2
+int elm = new Random().Next(10, 100);
+int count = CountElm(array, elm);
+PrintResult($"{count} элементов в заданном диапазоне");
