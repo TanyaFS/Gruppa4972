31cca39afd8857c4f1874ae05556be2b939e5f85
Tanya Fech <t_fech@mail.ru>
2023-09-22 16:28:24 +0300

    Added solutions to tasks 34,36,38

---
+++ Sem5Task36/Program.cs
@@ -0,0 +1,50 @@
+﻿// No36 Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+//Заполняем одномерный массив
+int[] GenArray(int length, int lowBorder, int highBorder)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(lowBorder, highBorder+1);
+    }
+
+    return array;
+}
+
+// Печатаем результат
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+        
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+// Считаем сумму элементов массива, стоящих на нечетных позициях
+int SumNegPos(int[] arr)
+{
+    int sum = 0;
+    
+    for (int i = 1; i < arr.Length; i=i+2)
+    {
+        sum = sum + arr[i];
+    }
+
+    return sum;
+}    
+
+// Выводим сообщение на консоль
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+int[] array = GenArray(123, 0, 1000);
+PrintArray(array);
+int sum = SumNegPos(array);
+PrintResult($"Сумма элементов, стоящих на нечётных позициях = {sum}");
