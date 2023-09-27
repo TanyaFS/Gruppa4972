c4b4a91a6bcbc38aaeeef7072c77c502fc831a9b
Tanya Fech <t_fech@mail.ru>
2023-09-27 17:05:43 +0300

    Solutions to Sem6 tasks 41,43

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,129 @@
+﻿// №41 Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел
+// больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+// Вариант 1
+// Ввод числа из консоли
+int ReadData(string msg)
+{
+    System.Console.Write(msg);
+    string value = Console.ReadLine() ?? "0";
+    int number = Convert.ToInt32(value);
+    return number;
+}
+
+int[] InputArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = ReadData($"Введите {i + 1}-й элемент: ");
+    }
+    return array;
+}
+
+// Печатаем результат
+void PrintArray(int[] arr)
+// Вариант 1
+/*{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"a[{i}] = {arr[i]}");
+    }
+}*/
+
+// Вариант 2
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+        
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+// Считаем количество положительных элементов в массиве
+int CountPositiveNumbers(int[] arr)
+{
+    int count = 0;
+   
+    for (int i = 0; i < arr.Length; i++)
+    {
+        // Проверяем, является ли текущий элемент положительным
+        if (arr[i] > 0)
+        {
+            // Если да, увеличиваем счетчик на 1
+            count++;
+        }
+    }
+    // Возвращаем значение счетчика
+    return count;
+}    
+
+// Выводим сообщение на консоль
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+int length = ReadData("Введите количество элементов: ");
+int[] array;
+array = InputArray(length);
+PrintArray(array);
+int count = CountPositiveNumbers(array);
+PrintResult($"Количество чисел больше 0 = {count}");
+
+// Вариант 2: регулярные выражения
+
+/*using System.Text.RegularExpressions;
+Console.Clear();
+
+int numOfKeyPresses = ReadInt("Введите количество нажатий: ");  // Ввод пользователем количества нажатий.
+string data = KeyPresses("Введите строку: ", numOfKeyPresses);  // Получение нажатий с клавиатуры.
+//Console.WriteLine(data);
+int count = CountPositiveNumbers(data);                         // Подсчет положительных чисел в строке.
+PrintResult($"\nВведено чисел больше нуля: {count}");           // Вывод результата в консоль.
+
+// Ввод целого числа с клавиатуры.
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Получение нажатий с клавиатуры.
+string KeyPresses(string msg, int numOfKeyPresses)
+{
+    Console.Write(msg);
+    string data = string.Empty;
+    
+    for (int i = 0; i < numOfKeyPresses; i++)
+    {
+        var keyInfo = Console.ReadKey();
+        data += keyInfo.KeyChar;
+    }
+
+    return data;
+}
+
+// Подсчет положительных чисел в строке с использованием регулярных выражений.
+int CountPositiveNumbers(string input)
+{
+    string pattern = @"[-]?[0-9]+";
+    MatchCollection matches = Regex.Matches(input, pattern);
+
+    int count = 0;
+    foreach (Match match in matches)
+    {
+        if (int.Parse(match.Value) > 0) { count++; }
+    }
+    return count;
+}
+
+// Вывод результата в консоль.
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}*/
---
+++ Sem6Task43/Program.cs
@@ -0,0 +1,35 @@
+﻿// №43 Напишите программу, которая найдёт точку пересечения двух прямых, заданных
+// уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются
+// пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+
+double k1 = ReadData("Введите коэффициент k1: ");
+double k2 = ReadData("Введите коэффициент k2: ");
+double b1 = ReadData("Введите коэффициент b1: ");
+double b2 = ReadData("Введите коэффициент b2: ");
+// Поиск координат пересечения
+(double x, double y) point = PointOfIntersection(k1, b1, k2, b2);
+double X = point.x;
+double Y = point.y;
+PrintData($"Точка пересечения: x= {X}; y= {Y}");
+
+// Метод считывания введенных данных
+double ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return double.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод вывода данных.\
+void PrintData(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+// Находим точку пересечения
+(double x, double y) PointOfIntersection (double k1, double b1, double k2, double b2)
+{
+    double X = (b2 - b1) / (k1 - k2);
+    double Y = (k1 * b2 - k2 * b1) / (k1 - k2);
+    return (X, Y);
+}
