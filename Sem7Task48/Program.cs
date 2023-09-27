bd125af5eb549ed8cbb8a9dcf220e0822a4f6aa0
Tanya Fech <t_fech@mail.ru>
2023-09-27 19:39:49 +0300

    Solutions to Sem 7 tasks 46,48, 49, 51

---
+++ Sem7Task48/Program.cs
@@ -0,0 +1,85 @@
+﻿// №48mЗадайте двумерный массив размера m на n, каждый
+// элемент в массиве находится по формуле: Aₘₙ =
+// m+n. Выведите полученный массив на экран.
+
+// Вариант 1
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+int[,] Fill2DSumInd(int row, int column)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i,j] = i+j;
+        }
+    }
+
+    return arr;
+}
+
+void Print2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j]+",\t  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int row = ReadData("Введите количество строк: ");
+int column = ReadData("Введите количество столбцов: ");
+
+int[,] arr2D = Fill2DSumInd(row,column);
+Print2DArray(arr2D);
+
+
+// Вариант 2
+// Вывод сообщения и запись введённых данных
+/*
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine()??",";
+    int number = Convert.ToInt32(value);
+
+    return number;
+}
+// Заполняет массив
+void FillingArray(int[,] array, int m, int n)
+{
+    for (int i = 0; i < m; i++)
+    for (int j = 0; j < n; j++)
+        array[i, j] = i + j;
+}
+// Выводит элементы массива в консоль
+void OutputArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("{");
+
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+            Console.Write(array[i, j] + ",\t ");
+
+        Console.WriteLine(array[i, array.GetLength(1) - 1] + "}");
+    }
+}
+
+int m = Prompt("Введите m: ");
+int n = Prompt("Введите n: ");
+
+int[,] array = new int[m, n];
+FillingArray(array, m, n);
+OutputArray(array);
+*/
---
+++ Sem7Task51/Program.cs
@@ -0,0 +1,55 @@
+﻿// №51 Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с
+// индексами (0,0); (1;1) и т.д.
+
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+int[,] Fill2DArray(int countRow, int countColumn, int topBorder, int downBorder)
+{
+    System.Random rnd = new System.Random();
+    int[,] array2D = new int[countRow, countColumn];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i,j] = rnd.Next(downBorder,topBorder);
+        }
+    }
+
+    return array2D;
+}
+
+// Ищем элементы, у которых оба индекса чётные и меняем их
+int MainDiagonalSum(int[,] array)
+{
+    int sum = 0;
+
+    for (int i = 0; i < array.GetLength(0) && i < array.GetLength(1); i++)
+        sum += array[i, i];
+
+    return sum;
+}
+
+void Print2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j]+",\t  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int row = ReadData("Введите количество строк: ");
+int column = ReadData("Введите количество столбцов: ");
+int[,] array = Fill2DArray(row,column, topBorder:100,downBorder:10);
+Print2DArray(array);
+Console.WriteLine("Сумма эл-тов главной диагонали: " + MainDiagonalSum(array));
